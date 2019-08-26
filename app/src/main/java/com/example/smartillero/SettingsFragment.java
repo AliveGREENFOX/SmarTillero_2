2f59acb3ea52004ceadfde793d31cf78a66a90d8
Omar <omar.gonzalez.favila@gmail.com>
2019-08-26 01:58:57 -0500

    Firebase funcionando (solo escritura), se añade bluetooth

--- app/src/main/java/com/example/smartillero/SettingsFragment.java
+++ app/src/main/java/com/example/smartillero/SettingsFragment.java
@@ -1,8 +1,10 @@
 package com.example.smartillero;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,12 +15,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingsFragment extends Fragment
 {
+    BluetoothAdapter BTA;
+    private static final String TAG = "Settings";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
@@ -26,10 +31,6 @@ public class SettingsFragment extends Fragment
 
         Switch switch1 = (Switch)view.findViewById(R.id.switch1);
        Button button_new = (Button)view.findViewById(R.id.button_new);
-
-
-
-
 
         button_new.setOnClickListener(new View.OnClickListener()
         {
@@ -43,12 +44,36 @@ public class SettingsFragment extends Fragment
             }
         });
 
-        if(switch1.isChecked())
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
         {
-            Toast.makeText(getActivity(),"Principal 2", Toast.LENGTH_SHORT).show();
-        }
-        //return inflater.inflate(R.layout.fragment_settings, container, false);
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                Toast.makeText(getActivity(),"Principal 2", Toast.LENGTH_SHORT).show();
+                //EnableDisableBT();
+            }
+        });
 
         return view;
+    }
+
+    public void EnableDisableBT()
+    {
+        if(BTA == null)     //If Bluetooth isn't available
+        {
+            Toast.makeText(getActivity(),"El dispositivo no cuenta con Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        if(!BTA.isEnabled()) //Enables BT
+        {
+            Intent EnableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(EnableBT);
+        }
+
+        if(BTA.isEnabled()) //Disables BT if enabled
+        {
+            BTA.disable();
+            IntentFilter BTintent = new IntentFilter(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
+        }
+
     }
 }
